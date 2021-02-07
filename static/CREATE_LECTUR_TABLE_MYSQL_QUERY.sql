992a97a361fc7d436431979e6094e6b27f9437cf
Jabar Basith <54178779+Jabarbasith@users.noreply.github.com>
2021-02-07 14:23:43 +0700

    Adding MySQL Query to create lectur table

---
+++ static/CREATE_LECTUR_TABLE_MYSQL_QUERY.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dosen (
+    nip VARCHAR(30) PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    alamat TEXT NOT NULL,
+    prodi VARCHAR(20) NOT NULL,
+    jurusan VARCHAR(20) NOT NULL,
+    no_telp VARCHAR(15) NOT NULL
+)
